a607bca44f28802b502cbce6b3871396fd5b13c9
luiz.leite <luiz.leite@hotmart.com>
2021-06-22 13:01:01 -0300

    add mongo entity

--- quarkus_mongodb/src/main/java/com/luizleiteoliveira/tutorials/domain/Book.java
+++ quarkus_mongodb/src/main/java/com/luizleiteoliveira/tutorials/domain/Book.java
@@ -1,7 +1,9 @@
 package com.luizleiteoliveira.tutorials.domain;
 
+import io.quarkus.mongodb.panache.MongoEntity;
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
 
+@MongoEntity(collection = "book")
 public class Book extends PanacheMongoEntity {
 
     public String name;
