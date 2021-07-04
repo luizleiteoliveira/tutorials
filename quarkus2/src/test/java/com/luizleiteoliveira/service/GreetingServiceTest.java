9488d0a7749ec68fb737aead0e9a9538dd449cc7
luiz.leite <luiz.leite@hotmart.com>
2021-07-04 17:37:35 -0300

    create book query for graphql

--- quarkus2/src/test/java/com/luizleiteoliveira/service/GreetingServiceTest.java
+++ quarkus2/src/test/java/com/luizleiteoliveira/service/GreetingServiceTest.java
@@ -2,7 +2,7 @@ package com.luizleiteoliveira.service;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GreetingServiceTest {
 
