7cd588d412d2e7885b025ef29c2f50b4120f2d6b
luiz.leite <luiz.leite@hotmart.com>
2021-07-13 07:55:35 -0300

    print content

--- aws_lambda_api_gateway/src/main/kotlin/com/luizleiteoliveira/tutorials/ApiGatewayHandler.kt
+++ aws_lambda_api_gateway/src/main/kotlin/com/luizleiteoliveira/tutorials/ApiGatewayHandler.kt
@@ -2,19 +2,31 @@ package com.luizleiteoliveira.tutorials
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.kxtra.slf4j.getLogger
+import java.io.BufferedReader
 import java.io.InputStream
+import java.io.InputStreamReader
 import java.io.OutputStream
+import org.kxtra.slf4j.getLogger
+
 
 class ApiGatewayHandler: RequestStreamHandler {
 
     companion object {
         private val LOGGER = getLogger()
-        private val mapper = ObjectMapper()
     }
 
     override fun handleRequest(inputStream: InputStream?, outputStream: OutputStream?, context: Context?) {
-        TODO("Not yet implemented")
+        val newLine = System.getProperty("line.separator")
+        val result = StringBuilder()
+        BufferedReader(InputStreamReader(inputStream)).use { reader ->
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                result
+                    .append(line)
+                    .append(newLine)
+            }
+        }
+        LOGGER.info(result.toString())
+
     }
 }
