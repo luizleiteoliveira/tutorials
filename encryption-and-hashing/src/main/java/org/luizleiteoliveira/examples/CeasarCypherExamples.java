7f5cd7c9f6190468f0ae98bdb8b81e76c6de1c6b
luiz.leite <luiz.leite@hotmart.com>
2022-08-28 14:01:12 -0300

    cracking ceasarcypher and improve encrypt

--- encryption-and-hashing/src/main/java/org/luizleiteoliveira/examples/CeasarCypherExamples.java
+++ encryption-and-hashing/src/main/java/org/luizleiteoliveira/examples/CeasarCypherExamples.java
@@ -1,14 +1,18 @@
 package org.luizleiteoliveira.examples;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CeasarCypherExamples {
 
     private static final String ALPHABETH = " ABCDEFGHIJKLMNOPQRSTUWXYZ";
 
     public String encrypt(String plainText, int key){
+        String upperCaseText = plainText.toUpperCase();
         StringBuilder encryptText = new StringBuilder();
         for (int charPosition = 0; charPosition < plainText.length(); charPosition++ ) {
-            char currentChar = plainText.charAt(charPosition);
+            char currentChar = upperCaseText.charAt(charPosition);
             encryptText.append(ALPHABETH.charAt(Math.floorMod(ALPHABETH.indexOf(currentChar) + key, ALPHABETH.length())));
         }
         return encryptText.toString();
@@ -25,5 +29,20 @@ public class CeasarCypherExamples {
 
         return decryptText.toString();
 
+    }
+
+    public List<String> crackCaesarCypher(String cypherText) {
+
+        List<String> allResults = new ArrayList<>();
+
+        for (int index = 0; index < ALPHABETH.length(); index++) {
+            StringBuilder decryptText = new StringBuilder();
+            for (int charPosition = 0; charPosition < cypherText.length(); charPosition++ ) {
+                char currentChar = cypherText.charAt(charPosition);
+                decryptText.append(ALPHABETH.charAt(Math.floorMod(ALPHABETH.indexOf(currentChar) - index, ALPHABETH.length())));
+            }
+            allResults.add(decryptText.toString());
+        }
+        return allResults;
     }
 }
--- encryption-and-hashing/src/test/java/org/luizleiteoliveira/examples/CeasarCypherExamplesTest.java
+++ encryption-and-hashing/src/test/java/org/luizleiteoliveira/examples/CeasarCypherExamplesTest.java
@@ -3,6 +3,9 @@ package org.luizleiteoliveira.examples;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Random;
+
 class CeasarCypherExamplesTest {
 
     private final CeasarCypherExamples ceasarCypherExamples = new CeasarCypherExamples();
@@ -27,18 +30,30 @@ class CeasarCypherExamplesTest {
     @Test
     public void checkOneLetterDecrypt(){
         String cypherText = "B";
-        String encryptResult = ceasarCypherExamples.decrypt(cypherText, 1);
-        Assertions.assertEquals("A", encryptResult);
+        String decryptResult = ceasarCypherExamples.decrypt(cypherText, 1);
+        Assertions.assertEquals("A", decryptResult);
         cypherText = " ";
-        encryptResult = ceasarCypherExamples.decrypt(cypherText, 1);
-        Assertions.assertEquals("Z", encryptResult);
+        decryptResult = ceasarCypherExamples.decrypt(cypherText, 1);
+        Assertions.assertEquals("Z", decryptResult);
     }
 
     @Test
     public void testWordDecrypt() {
         String plainText = "BNPS";
-        String encryptResult = ceasarCypherExamples.decrypt(plainText, 1);
-        Assertions.assertEquals("AMOR", encryptResult);
+        String decryptResult = ceasarCypherExamples.decrypt(plainText, 1);
+        Assertions.assertEquals("AMOR", decryptResult);
+    }
+
+    @Test
+    public void crackTest() {
+
+        String message = "This is a secret message";
+        int random = new Random().ints(0, 90).findAny().getAsInt();
+        String encryptMessage = ceasarCypherExamples.encrypt(message, random);
+        List<String> allResults = ceasarCypherExamples.crackCaesarCypher(encryptMessage);
+        Assertions.assertTrue(allResults.contains(message.toUpperCase()));
+
+
     }
 
 }
