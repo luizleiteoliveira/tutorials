a31bc4c736638426143d2f4d0f6c065b4a78f899
luiz.leite <luiz.leite@hotmart.com>
2023-05-25 13:05:12 -0300

    start project

---
+++ test-crac/src/main/java/com/luizleiteoliveira/testcrac/TestCracApplication.java
@@ -0,0 +1,13 @@
+package com.luizleiteoliveira.testcrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCracApplication.class, args);
+	}
+
+}
