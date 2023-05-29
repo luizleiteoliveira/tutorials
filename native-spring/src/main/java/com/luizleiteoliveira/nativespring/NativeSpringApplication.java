c8aad419a0541797b79401da077833762addf179
luiz.leite <luiz.leite@hotmart.com>
2023-05-29 14:05:18 -0300

    native-spring-poc

---
+++ native-spring/src/main/java/com/luizleiteoliveira/nativespring/NativeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.luizleiteoliveira.nativespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeSpringApplication.class, args);
+	}
+
+}
---
+++ native-spring/src/test/java/com/luizleiteoliveira/nativespring/NativeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizleiteoliveira.nativespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
