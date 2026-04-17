30e0f987fbad5256b3f0b0915c1b68e982dd1ca4
luizleite-hotmart <luiz.leite@hotmart.com>
2026-04-17 05:47:13 -0300

    Initialize Spring Boot project for base REST API with Maven Wrapper and JDK 25

---
+++ base-rest-api-jdk25/src/main/java/com/luizleiteoliveira/baserestapijdk25/BaseRestApiJdk25Application.java
@@ -0,0 +1,13 @@
+package com.luizleiteoliveira.baserestapijdk25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseRestApiJdk25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseRestApiJdk25Application.class, args);
+    }
+
+}
---
+++ base-rest-api-jdk25/src/test/java/com/luizleiteoliveira/baserestapijdk25/BaseRestApiJdk25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizleiteoliveira.baserestapijdk25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseRestApiJdk25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
