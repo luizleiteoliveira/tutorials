e3709154c99c4008c680b5957e2fd4dea8528ed1
luizleite-hotmart <luiz.leite@hotmart.com>
2026-04-17 06:06:10 -0300

    adding cache

--- base-rest-api-jdk25/src/main/java/com/luizleiteoliveira/baserestapijdk25/BaseRestApiJdk25Application.java
+++ base-rest-api-jdk25/src/main/java/com/luizleiteoliveira/baserestapijdk25/BaseRestApiJdk25Application.java
@@ -2,8 +2,10 @@ package com.luizleiteoliveira.baserestapijdk25;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BaseRestApiJdk25Application {
 
     public static void main(String[] args) {
